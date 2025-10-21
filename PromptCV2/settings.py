b8e5ac7acbecdd6c3ba52c4064563a357b1391c2
Bharathikannan R <r.bharathikannan.official@gmail.com>
2025-10-21 11:44:50 +0530

    Update settings.py

--- PromptCV2/settings.py
+++ PromptCV2/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-az$&7w2v$_8l((@nxh7*)k3-airc(vkv2_)!os+th)clx!&2%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
